d1dd941ff5428be085f628b1cf17acad22cf00e2
Rhian <12019940@stu.mmu.ac.uk>
2015-11-12 13:59:20 +0000

    Update AssignmentCipher.py

--- AssignmentCipher.py
+++ AssignmentCipher.py
@@ -4,17 +4,17 @@ import reverseCipher
 
 myMessage = raw_input('Enter Message   : ') 
 myKey = raw_input('Enter Key   :  ') 
-myMode = raw_input('E or D   :   ') 
+myMode = raw_input('e or d   :   ') 
 
-if myMode == 'E':
-	for i in range(64):
+if myMode == 'e':
+	for i in range(32):
 		translated = vigenereCipher.encryptMessage(myKey, myMessage)
 		translated1 = caesarCipher.encryptMessage(len(myKey), translated)
 		myMessage = reverseCipher.reverse(translated1)
 	print myMessage
 
-elif myMode == 'D':
-	for i in range(64):		
+elif myMode == 'd':
+	for i in range(32):		
 		translated = reverseCipher.reverse(myMessage)
 		translated1 = caesarCipher.decryptMessage(len(myKey), translated)
 		myMessage = vigenereCipher.decryptMessage(myKey, translated1)	
