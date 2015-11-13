6ead6e7f206fc803664d0bbe62bea116699bf127
Rhian <12019940@stu.mmu.ac.uk>
2015-11-13 11:24:27 +0000

    Update AssignmentCipher.py

--- AssignmentCipher.py
+++ AssignmentCipher.py
@@ -7,24 +7,22 @@ myKey = raw_input('Enter First Key   :  ')
 myKey1 = raw_input('Enter Second Key   :  ')  
 myMode = raw_input('E or D  :   ') 
 
-if myMode == 'E':
+if myMode.lower() == 'e':
 	for i in range(32):
 		translated = vigenereCipher.encryptMessage(myKey, myMessage)
 		translated1 = caesarCipher.encryptMessage(len(myKey1), translated)
 		myMessage = reverseCipher.reverse(translated1)
 	print myMessage
 
-elif myMode == 'D':
+elif myMode.lower() == 'd':
 	for i in range(32):		
 		translated = reverseCipher.reverse(myMessage)
 		translated1 = caesarCipher.decryptMessage(len(myKey1), translated)
 		myMessage = vigenereCipher.decryptMessage(myKey, translated1)	
 	print myMessage
 
-def main(m, k, om. k1):
+def main(m, k, om, k1):
 	myKey = k
 	myKey1 = k1
 	myMode = om
 	myMessage = m
-
-
